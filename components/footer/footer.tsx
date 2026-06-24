6c405600ed41286f0c7de9292fa6098ac6432e88
johanPatiño <jpatinog6378@universidadean.edu.co>
2026-06-24 10:41:44 -0500

    Fix mail and visual

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -17,7 +17,7 @@ const InstagramIcon = () => (
 
 
 export function Footer() {
-  const email = "contact@accllc.co"; // Reemplaza esto cuando tengas el email final
+  const email = "admin@accllc.co"; // Reemplaza esto cuando tengas el email final
   const phone = "+1 (786) 580-2979"; 
   const phone2 = "+1 (561) 698 2828";
   const facebookUrl = "https://www.facebook.com/AllianceCarsCompanyOficial";
